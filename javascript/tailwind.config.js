2221b4f20aa00e3f6f95bc2ce0eceafb16906a0d
moshiurhossain <pantho883@gmail.com>
2023-10-05 16:43:03 +0400

    first commit

---
+++ javascript/tailwind.config.js
@@ -0,0 +1,37 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+        //  color start
+         headcolor:"#1C1E53",
+         headbuttonborder:"#F4F6FC",
+         buttoncolor1:"#FCD980",
+         weWorkFontColor:"#282938",
+         linkColor:"#2405F2",
+         bannerbg:"rgba(28, 30, 83, 0.656)",
+         part5bg:"rgba(244, 246, 252, 0.5)",
+         //  color end
+        },
+        // containers start
+        width: {
+           navdiv:"1285px",
+            
+          },
+          // containers end
+          // font family start
+          fontFamily: {
+            
+          pops:['Poppins', 'sans-serif'],
+            
+          },
+          // font family end
+          // background image start
+          backgroundImage: {
+            bannerbg2:"url('images/ww.png')",
+          },
+           // background image start
+      },
+    },
+  };
+
+  
